a2c58773975fd718cea94662de45b9deb9b728a0
marialago <maria.lago@radixeng.com.br>
2022-05-25 15:53:19 -0300

    implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { NovaTRansferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovaTRansferenciaComponent
   ],
   imports: [
     BrowserModule
