09daacdf62483f0cf83bc0951a21b0b3c48b386c
marialago <maria.lago@radixeng.com.br>
2022-05-26 18:22:05 -0300

    3. Exportando dados do componente

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector:'app-nova-transferencia',
@@ -6,13 +6,22 @@ import { Component } from "@angular/core";
     styleUrls:['./nova-transferencia.component.scss']
 
 })
-export class NovaTransferenciaComponent{
+export class NovaTransferenciaComponent {
+
+  @Output() aoTransferir = new EventEmitter<any>();
 
   valor: number;
   destino: number;
-  transferir(){
+
+  transferir() {
     console.log('Solicitada nova transferência');
-    console.log('valor:', this.valor);
-    console.log('destino: ', this.destino);
+    const valorEmitir = {valor: this.valor, destino: this.destino};
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
   }
 }
